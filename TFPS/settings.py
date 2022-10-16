cde506e9d3d26c32bb290c9d91f1b669460a72d6
Josh Hehir <hehir24@gmail.com>
2022-10-16 17:27:36 +1100

    Added comments
    - Added line comments to explain various functions

--- TFPS/settings.py
+++ TFPS/settings.py
@@ -1,9 +1,11 @@
 import json
 
+"""Adds the settings into a directory"""
 settings: {}
 with open('config.json', 'r') as f:
     settings = json.load(f)
 
 
 def get_setting(key):
+    """Gets the requested settings key and returns a string"""
     return settings[key]
